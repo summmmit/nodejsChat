aeae18e03bdd49befd43fbef6183356cf9656443
Sumit Singh <sumit_prasad@epark.co.jp>
2015-12-16 17:34:34 +0900

    half finished

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
-/**
- * Created by 1084760 on 2015/12/16.
- */
+var mongo = require('mongodb').MongoClient;
+var client = require('socket.io').listen(8080).sockets;
 
-console.log("hello");
+mongo.connect('mongodb://127.0.0.1/nodejsChat', function (err, db) {
+
+    if (err)
+        throw err;
+
+    client.on('connection', function (socket) {
+
+        console.log('some is helre');
+
+        var col = db.collection('messages');
+
+        // wait for input
+        socket.on('input', function (data) {
+            console.log('1');
+            console.log(data);
+
+            var name = data.name;
+            var message = data.message;
+            var whiteSpacePattern = /^\s*$/;
+
+            if(whiteSpacePattern.test(name) || whiteSpacePattern.test(message)){
+                console.log('Invalid data');
+            }else {
+
+                col.insert({name: name, message: message}, function(inputData){
+                    console.log(inputData);
+                })
+            }
+        })
+
+    });
+});
